d59793459dadd10328d9c0fcf08496fab7984c9a
Aftab Mumtaz <aftabmumtaz14@gmail.com>
2026-08-03 17:11:45 +0500

    as

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const app = express();
 
 // Security and utility middlewares
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
